782359c873bae57acb6d61410aff80790c2c7c2d
anhhv <anhhv@gotech.vn>
2023-03-17 08:45:26 +0700

    Init Project

---
+++ app/src/main/java/anhhv/colors_picker/MainActivity.kt
@@ -0,0 +1,11 @@
+package anhhv.colors_picker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
